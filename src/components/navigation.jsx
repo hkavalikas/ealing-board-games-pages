dd0c9f780708d10c90dffb325b33b650c55640a7
Harry Kavalikas <harry.kavalikas@hotmail.com>
2023-03-19 20:45:08 +0000

    Cleanup

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            Ealing Board Games Club
+            Ealing Board Games
           </a>{" "}
         </div>
 
