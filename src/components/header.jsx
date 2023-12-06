3405b86dbe0b7584c166422ecf247e7dd794f96b
Harry Kavalikas <harry.kavalikas@hotmail.com>
2023-12-06 21:44:00 +0000

    feat: lazy load image and compress mp4

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,33 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import introVideo from '../animation-loop.mp4';
 
 export const Header = (props) => {
-  return (
-    <header id="header">
-      <div className="intro">
-      <video className='intro' autoPlay loop muted>
-        <source src={introVideo} type='video/mp4' />
-      </video>
-        {/* <div className="overlay">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-8 col-md-offset-2 intro-text">
-                <h1>
-                  {props.data ? props.data.title : "Loading"}
-                  <span></span>
-                </h1>
-                <p>{props.data ? props.data.paragraph : "Loading"}</p>
-                <a
-                  href="https://www.facebook.com/groups/ealingboardgames"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
-                  Facebook group
-                </a>{" "}
-              </div>
+    useEffect(() => {
+        const video = document.querySelector('.intro video');
+
+        const loadVideo = () => {
+            if (video) {
+                video.src = introVideo;
+            }
+        };
+
+        window.addEventListener('load', loadVideo);
+
+        return () => {
+            window.removeEventListener('load', loadVideo);
+        };
+    }, []);
+
+    return (
+        <header id="header">
+            <div className="intro">
+                <video className='intro' autoPlay loop muted>
+                    {/* Remove the 'src' attribute from here */}
+                </video>
             </div>
-          </div>
-        </div> */}
-      </div>
-    </header>
-  );
+        </header>
+    );
 };
