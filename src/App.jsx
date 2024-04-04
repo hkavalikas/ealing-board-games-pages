a6ec1151d6edc03d38bb6d7626a22f8cd8007766
Harry Kavalikas <harry.kavalikas@hotmail.com>
2024-04-04 21:56:30 +0100

    fix: indentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { Header } from "./components/header";
-import { Contact } from "./components/contact";
+import React, {useState, useEffect} from "react";
+import {Header} from "./components/header";
+import {Contact} from "./components/contact";
 import JsonData from "./resources/data/data.json";
 
 
 const App = () => {
-  const [landingPageData, setLandingPageData] = useState({});
-  useEffect(() => {
-    setLandingPageData(JsonData);
-  }, []);
+    const [landingPageData, setLandingPageData] = useState({});
+    useEffect(() => {
+        setLandingPageData(JsonData);
+    }, []);
 
-  return (
-    <div>
-      <Header data={landingPageData.Header} />
-      <Contact data={landingPageData.Contact} />
-    </div>
-  );
+    return (<div>
+            <Header data={landingPageData.Header}/>
+            <Contact data={landingPageData.Contact}/>
+        </div>);
 };
 
 export default App;
